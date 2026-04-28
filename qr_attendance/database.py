ced5de8f025d58c64a2534a91038928d41392575
jacoballyson08 <jacoballyson08@gmail.com>
2026-04-28 11:19:46 +0800

    database.py

--- qr_attendance/database.py
+++ qr_attendance/database.py
@@ -1,10 +1,12 @@
-import mysql.connector
-
+import psycopg2
+import os
 
 def get_db():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="qr_attendance_db"
+    conn = psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT")
     )
+    return conn
